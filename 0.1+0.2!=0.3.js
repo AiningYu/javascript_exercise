8132e8943459cbb15f95a07ef41dc9cb4883c955
AiningYu <effiaidd@gmail.com>
2024-10-09 17:48:39 +0800

    add 0.1+0.2!=0.3.js test

--- 0.1+0.2!=0.3.js
+++ 0.1+0.2!=0.3.js
@@ -1,4 +1,4 @@
-//转换成整数计算
+//1. 转换成整数计算
 function caulculator(a, b) {
     let factor = Math.pow(10, Math.max(decimalcount(a), decimalcount(b)));
     return (a*factor+b*factor)/factor;
@@ -14,3 +14,9 @@ function decimalcount(a) {
 
 console.log(0.1+0.2);
 console.log(caulculator(0.1, 0.2));
+
+//2.使用第三方库 Decimal.js
+const Decimal = require('decimal.js');
+let a = new Decimal(0.1);
+let b = new Decimal(0.2);
+console.log(a.plus(b).toString());
